63a7afdaf452d71a96696b302a064ae3766c44c8
masimmursaleen-hub <masimmursaleen@gmail.com>
2025-09-20 22:37:34 +0500

    server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-const API_KEY = "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";
+const API_KEY = process.env.GROQ_API_KEY;
 
 app.post("/chat", async (req, res) => {
   const history = req.body.history;
@@ -46,3 +46,4 @@ app.post("/chat", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
