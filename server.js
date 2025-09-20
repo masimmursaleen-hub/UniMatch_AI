0c70098ec5c4bdf426656bf4a718f6a1f673d2cc
masimmursaleen-hub <masimmursaleen@gmail.com>
2025-09-20 22:41:39 +0500

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // ye env file read karega
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const API_KEY = process.env.API_KEY; // env se read hoga
 
 app.use(cors());
 app.use(express.json());
-
-const API_KEY = process.env.GROQ_API_KEY;
 
 app.post("/chat", async (req, res) => {
   const history = req.body.history;
@@ -46,4 +48,3 @@ app.post("/chat", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-
